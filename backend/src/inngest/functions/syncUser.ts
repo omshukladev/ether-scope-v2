2b4478c40d9f3ef2621038fe911a69bb6e701459
Om Shukla <omshuklalko@gmail.com>
2026-03-07 18:42:30 +0530

    fix(clerkRoute):fixed clerk route 07

--- backend/src/inngest/functions/syncUser.ts
+++ backend/src/inngest/functions/syncUser.ts
@@ -3,16 +3,16 @@ import { inngest } from "../client";
 export const syncUser = inngest.createFunction(
   { id: "sync-clerk-user" },
   { event: "clerk/user.created" },
-  async ({ event, step, env }: any) => {
+  async ({ event, step }) => {
     const user = event.data;
 
-    await step.run("insert-user", async () => {
+    await step.run("insert-user", {}, async ({ env }) => {
       await env.DB.prepare(
         `
-        INSERT INTO users (id,name,email,profile_image,created_at)
-        VALUES (?, ?, ?, ?, ?)
-        ON CONFLICT(id) DO NOTHING
-      `,
+          INSERT INTO users (id,name,email,profile_image,created_at)
+          VALUES (?, ?, ?, ?, ?)
+          ON CONFLICT(id) DO NOTHING
+        `,
       )
         .bind(
           user.id,

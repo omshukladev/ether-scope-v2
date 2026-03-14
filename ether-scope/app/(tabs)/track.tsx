76528275a595a8db135c47a0403815dd4fc45da0
Om Shukla <omshuklalko@gmail.com>
2026-03-14 10:45:50 +0530

    fix(ui):fixed notification

--- ether-scope/app/(tabs)/track.tsx
+++ ether-scope/app/(tabs)/track.tsx
@@ -24,7 +24,9 @@ Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
     shouldPlaySound: true,
-    shouldSetBadge: false,
+    shouldSetBadge: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
   }),
 });
 
